26862b6c7481016cb86e6f439d74dc78ff06e6d8
jonz94 <jody16888@gmail.com>
2022-05-12 09:15:16 +0800

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+function close() {
+  window.close()
+}
+</script>
+
+<template>
+  <button @click="close()">window.close()</button>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
